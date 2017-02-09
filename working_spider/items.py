bd2a4c81f5ef5f5a61a7564d59b21759edafe2f3
pkonarzewski <pa.ko@wp.pl>
2017-02-09 23:24:43 +0100

    modfiy OfferItem

--- working_spider/items.py
+++ working_spider/items.py
@@ -1,18 +1,13 @@
 # -*- coding: utf-8 -*-
-
-# Define here the models for your scraped items
-#
-# See documentation in:
-# http://doc.scrapy.org/en/latest/topics/items.html
-
 import scrapy
 
 
-class JobOffer(scrapy.Item):
+class OfferItem(scrapy.Item):
     offer_id = scrapy.Field()
-    offer_link = scrapy.Field()
     job_title = scrapy.Field()
     job_desc = scrapy.Field()
     job_loc = scrapy.Field()
-    employer_name = scrapy.Field()
-    add_date = scrapy.Field()
+    job_posted = scrapy.Field()
+    employeer = scrapy.Field()
+    link = scrapy.Field()
+    portal = scrapy.Field()
