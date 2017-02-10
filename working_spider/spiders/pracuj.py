a44785fbc825c03a020c8e047ac849f20e2a2dbb
pkonarzewski <p.konarzewski@capitalservice.pl>
2017-02-10 12:51:13 +0100

    format

--- working_spider/spiders/pracuj.py
+++ working_spider/spiders/pracuj.py
@@ -15,17 +15,16 @@ class QuotesSpider(scrapy.Spider):
     def parse(self, response):
         xpath_offer = '//ul[@id="mainOfferList"]//li[contains(@itemtype, "http://schema.org/JobPosting")]'
         for index, offer in enumerate(response.xpath(xpath_offer), 1):
-            joboffer = OfferItem(
-                offer_id=int(offer.xpath('h2').re(r'data-applied-offer-id="(\d+)"')[0]),
+            yield OfferItem(
+                offer_id=offer.xpath('h2').re(r'data-applied-offer-id="(\d+)"')[0],
                 job_title=offer.xpath('h2/a/text()').extract_first(),
                 job_desc=offer.xpath('div//p[contains(@class, "o-list_item_text_details")]/text()').extract_first(),
                 job_loc=offer.xpath('p//span[contains(@class, "o-list_item_desc_location_name_text")]/text()').extract_first(),
                 post_date=None,  # TODO: add
                 employer_name=offer.xpath('h3/a/text()').extract_first(),
-                offer_link='https://www.pracuj.pl{}'.format(offer.xpath('h2/a/@href').extract_first()),
+                offer_link=response.urljoin(offer.xpath('h2/a/@href').extract_first()),
                 portal='pracuj.pl'
             )
-            yield joboffer
 
         # pagination
         next_page = response.xpath('//ul[contains(@class, "desktopPagin")]/li/a[contains(text(), "Następna")]/@href').extract_first()
