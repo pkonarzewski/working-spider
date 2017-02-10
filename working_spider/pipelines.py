2caaf9b0484f45386ee2602d9aadfc7b9b36617f
pkonarzewski <p.konarzewski@capitalservice.pl>
2017-02-10 13:26:54 +0100

    add cleaning pipeline

--- working_spider/pipelines.py
+++ working_spider/pipelines.py
@@ -1,11 +1,23 @@
 # -*- coding: utf-8 -*-
 
-# Define your item pipelines here
-#
-# Don't forget to add your pipeline to the ITEM_PIPELINES setting
-# See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
 
+class OfferFormater(object):
 
-class WorkingSpiderPipeline(object):
+    def spider_opened(self, spider):
+        pass
+
+    def spider_closed(self, spider):
+        pass
+
     def process_item(self, item, spider):
+
+        item['offer_id'] = int(item['offer_id'].strip())
+        item['job_title'] = item['job_title'].strip()
+        item['job_desc'] = item['job_desc'].strip()
+        item['job_loc'] = item['job_loc'].strip()
+        item['post_date'] = item['post_date'].strip()
+        item['employer'] = item['employer'].strip()
+        item['offer_link'] = item['offer_link'].strip()
+        item['portal'] = item['portal'].strip()
+
         return item
